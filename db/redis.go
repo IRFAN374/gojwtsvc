bb4eebaeb7a381f30a87a44382a699864f4feb03
irfanamu <irfan@valuecreatives.com>
2022-11-28 23:49:09 +0530

    redis db is added

---
+++ db/redis.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"os"
+
+	"github.com/go-redis/redis"
+)
+
+var client *redis.Client
+
+func CnnectRedis() *redis.Client {
+	//Initializing redis
+	dsn := os.Getenv("REDIS_DSN")
+	if len(dsn) == 0 {
+		dsn = "localhost:6379"
+	}
+	client = redis.NewClient(&redis.Options{
+		Addr: dsn, //redis port
+	})
+	_, err := client.Ping().Result()
+	if err != nil {
+		panic(err)
+	}
+
+	return client
+}
